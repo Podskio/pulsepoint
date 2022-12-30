c624792de8de5d578ced2c7c48c6a8e099d934a3
JPod32 <jpodcat@gmail.com>
2022-12-29 19:41:40 -0800

    Improve documentation and keywords

--- src/crypto.ts
+++ src/crypto.ts
@@ -5,6 +5,7 @@ import type { APIData } from "./types"
 const hashPassword = "tombrady5rings" // Someone likes football...
 
 const getHashComponents = (data: APIData) => {
+  // Convert data to necessary formats
   const cipherText = Buffer.from(data.ct, "base64")
   const initVector = Buffer.from(data.iv, "hex")
   const salt = Buffer.from(data.s, "hex")
@@ -21,6 +22,7 @@ export const decodeJson = (data: APIData) => {
   let intermediateHash = null
   let key = Buffer.alloc(0)
 
+  // Derive key
   while (key.length < 32) {
     if (intermediateHash) hash.update(intermediateHash)
 
@@ -32,12 +34,16 @@ export const decodeJson = (data: APIData) => {
     key = Buffer.concat([key, intermediateHash])
   }
 
+  // Decipher using key
   const decipher = createDecipheriv("aes-256-cbc", key, initVector)
 
   let output = decipher.update(cipherText)
   output = Buffer.concat([output, decipher.final()])
 
+  // Remove leading and trailing quotes
   let result = output.toString().slice(1, -1)
+
+  // Replace escaped quotes with actual quotes
   result = result.replaceAll(/\\"/g, '"')
 
   try {
